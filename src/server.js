fccb64c58a36072d878a35328539f563b68b0809
Gustavo Hideki <gustavohideki@outlook.com>
2022-07-02 06:15:32 -0300

    Finaliza POST cadastro e POST login sem token

--- src/server.js
+++ src/server.js
@@ -3,8 +3,86 @@ import joi from "joi";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
+import bcrypt from "bcrypt";
 
 dotenv.config();
 
-console.log(process.env.MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+const loginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required()
+});
+
+//POST Login
+server.post("/login", async(req, res) => {
+    const { email, password } = req.body;
+
+    const validate = loginSchema.validate({ email, password });
+
+    if (validate.error) {
+        res.sendStatus(422)
+        return;
+    } else {
+        try {
+            await mongoClient.connect();
+            const db = mongoClient.db("wallet");
+            const usersCollection = db.collection("users");
+            const user = await usersCollection.findOne({ email: email });
+
+            if (!user) {
+                res.sendStatus(404);
+                return;
+            } else if (bcrypt.compareSync(password, user.password)) {
+                res.sendStatus(200);
+                return;
+            }
+        } catch(error) {
+            res.sendStatus(500);
+            return;
+        }
+        
+    }
+});
+
+//POST Cadastro
+server.post("/cadastro", async(req, res) => {
+    const userSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required(),
+        passwordCheck: joi.string().required()
+    });
+
+    const { name, email, password, passwordCheck } = req.body;
+
+    const validate = userSchema.validate(req.body);
+
+    const doPasswordsCheck = (password === passwordCheck);
+
+    if (validate.error) {
+        res.sendStatus(422);
+        return;
+    } else if (doPasswordsCheck) {
+        try {
+            await mongoClient.connect();
+            const db = mongoClient.db("wallet");
+            const usersCollection = db.collection("users");
+            const encryptedPassword = bcrypt.hashSync(password, 10);
+            await usersCollection.insertOne({ name, email, password: encryptedPassword });
+            res.sendStatus(201);
+            return;
+        } catch(error) {
+            res.sendStatus(500);
+            return;
+        }
+    }
+});
+
+
+server.listen(5000, ()=>{console.log("Servidor rodando!")});
